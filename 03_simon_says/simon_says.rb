d493cef5ab8991aa068f9e790c5d68e073a5c39b
AnkitGeorge <ankit.george@gmail.com>
2016-01-18 17:06:57 -0500

    Finish 03_simon says program.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -16,7 +16,7 @@ def repeat(input_string, num = 2)
     output_string << input_string
     counter += 1
   end
-  output_string.join(" ")
+  output_string.join(" ") #combines string, separated with spaces.
 end
 
 def start_of_word(word, num)
@@ -33,3 +33,33 @@ def start_of_word(word, num)
   #
   # output_part_of_word.join
 end
+
+def first_word(firstword)
+  ary1 = firstword.split
+  ary1[0]
+  #or firstword.split.first
+end
+
+def titleize(sentence)
+  prepositions = ["the", "or", "and", "over"]
+  titleized_string = []
+
+  titleized_string = sentence.split.each_with_index { |word, index|
+    word.capitalize! unless prepositions.include?(word) && index != 0
+  }       #creates an array of strings with the input sentence broken up
+          #and and index which capitalize works with unless conditions
+
+  titleized_string.join(" ") #convert to string with spaces between words
+
+end
+
+# def titleize(sentence)
+#  prepositions = ["the", "or", "and", "over"]
+#  titleized_string = ""
+#
+#  titleized_string = sentence.split(" ").each_with_index {|word, index|
+#    word.capitalize! unless prepositions.include?(word) && index != 0
+#  }
+#
+#  titleized_string.join(" ")
+# end
